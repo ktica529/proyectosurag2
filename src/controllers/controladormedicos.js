f783483ca042621826ee6041d54007ab3d139f83
cata_nc <cataramirezmesa@gmail.com>
2022-11-30 17:43:51 -0500

    medicos

--- src/controllers/controladormedicos.js
+++ src/controllers/controladormedicos.js
@@ -5,26 +5,24 @@
 // }
 // console.log(nombre)
 
+//IMPORTACIONES
+import { capturaDatos } from '../formularios/formulariomedico.js'
+
+//Etiquetas que controlo:
 let etiquetaNombre=document.getElementById("nombres")
 let etiquetaBoton=document.getElementById("botonenvio")
 let etiquetaDocumento=document.getElementById("documento")
+let etiquetaespecialidad=document.getElementById("especialidad")
+let etiquetaRegistroProfesional=document.getElementById("RegistroProfesional")
+let etiquetaCorreoElectronico=document.getElementById("CorreoElectronico")
+let etiquetasede=document.getElementById("sede")
+let etiquetahorario=document.getElementById("horario")
+let etiquetadescripcion=document.getElementById("descripcion")
+let etiquetafotografia=document.getElementById("fotografia")
 
 //detectar al evento de clic
 etiquetaBoton.addEventListener("click",function(evento){
 
     evento.preventDefault()
-    /*Swal.fire(
-        'Exito en el registro',
-        'El especialista ha sido registrado',
-        'success'
-      )*/
-
-
-      //Recibiendo los datos del formulario
-      let nombresMedico=etiquetaNombre.value
-      console.log(nombresMedico)
-
-      let documentoMedico=etiquetaDocumento.value
-      console.log(documentoMedico)
-
+    capturaDatos(etiquetaNombre,etiquetaDocumento,etiquetaespecialidad,etiquetaRegistroProfesional,etiquetaCorreoElectronico,etiquetasede,etiquetahorario,etiquetadescripcion,etiquetafotografia)   
 })
---
+++ src/formularios/formulariomedico.js
@@ -0,0 +1,45 @@
+import {validarFormulario} from './validacionformulariomedico.js'
+
+//creamos una funcion que nos permita recibir los datos del formulario
+export function capturaDatos(etiquetaNombre,etiquetaDocumento,etiquetaespecialidad,etiquetaRegistroProfesional,etiquetaCorreoElectronico,etiquetasede,etiquetahorario,etiquetadescripcion,etiquetafotografia){
+
+    let nombresMedico=etiquetaNombre.value
+    let documentoMedico=etiquetaDocumento.value
+    let especialidadMedico=etiquetaespecialidad.value
+    let RegistroProfesional=etiquetaRegistroProfesional.value
+    let CorreoElectronico=etiquetaCorreoElectronico.value
+    let sede=etiquetasede.value
+    let horario=etiquetahorario.value
+    let descripcion=etiquetadescripcion.value
+    let fotografia=etiquetafotografia.value
+
+    // //console.log(nombresMedico)
+    // console.log(documentoMedico)
+    // console.log(especialidadMedico)
+    // console.log(RegistroProfesional)
+    // console.log(CorreoElectronico)
+    // console.log(sede)
+    // console.log(horario)
+    // console.log(descripcion)
+    // console.log(fotografia)
+
+    //debemos almacenar todos los valores del formulario
+    // en una Sola VARIABLE
+    // OBJETO=ALAMCENAR VARIOS DATOS EN 1 SOLO ESPACIO DE MEMORIA 
+    let datosFormularioMedico={
+        nombre:nombresMedico,
+        documento:documentoMedico,
+        especialidad:especialidadMedico,
+        Registro:RegistroProfesional,
+        Correo:CorreoElectronico,
+        sede:sede,
+        horario:horario,
+        descripcion:descripcion,
+        foto:fotografia,
+    }
+    console.log(datosFormularioMedico)
+
+
+
+
+}
